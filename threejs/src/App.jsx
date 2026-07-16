10c7b1f86f299b1316d0d2973d19e697610e4082
arunz6 <arunrajput88176894@gmail.com>
2026-07-16 10:13:15 +0530

    feat: initialize 3D scene with GLTF model rendering and GSAP animations

--- threejs/src/App.jsx
+++ threejs/src/App.jsx
@@ -4,10 +4,16 @@ import Exper from "./component/Exper";
 function App() {
   return (
     <>
-      <div className="parent h-screen w-screen bg-black">
-        <Canvas>  
+      <div className="parent h-screen w-screen  bg-black">
+      <div className="3dbg z-0 h-screen w-screen flex  items-center justify-center absolute">
+          <Canvas>  
           <Exper />
         </Canvas>
+      </div>
+       <div className="z-50   absolute top-10 left-10">
+        <h1 className="text-5xl mb-4 text-white ">hello</h1>
+        <h1 className="text-5xl font-bold text-white ">fullstack </h1>
+        </div>
       </div>
     </>
   );
--- threejs/src/component/Exper.jsx
+++ threejs/src/component/Exper.jsx
@@ -1,17 +1,38 @@
-import { OrbitControls } from '@react-three/drei'
-import React from 'react'
+import { useGLTF, OrbitControls } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
+gsap.registerPlugin(ScrollTrigger);
 
 const Exper = () => {
+  const { scene } = useGLTF("/shape.glb");
+  const mesh = useRef(null);
+
+  useEffect(() => {
+    gsap.to(mesh.current.rotation, {
+      y: Math.PI * 2,
+      x: Math.PI*3,
+      duration: 100,
+      repeat: -1,
+      ease: 'linear',
+    });
+    gsap.to(mesh.current.material, {
+      opacity: 1,
+      duration: 2,
+      ease: "power2.out",
+    });
+  });
   return (
     <>
-      <ambientLight intensity={3} />
-      <OrbitControls/>
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
+      <ambientLight intensity={2} />
+      <directionalLight position={[10, 10, 5]} intensity={1} castShadow />
+      <directionalLight position={[5, 2, 10]} intensity={1} castShadow />
+      <directionalLight position={[-10, 10, 10]} intensity={1} castShadow />
+      <directionalLight position={[-10, -10, -10]} intensity={1} castShadow />
+<OrbitControls/>
+      <primitive object={scene} ref={mesh} material={mesh.material} scale={2.1} />
     </>
-  )
-}
+  );
+};
 
-export default Exper
+export default Exper;
